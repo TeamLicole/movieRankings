95a90a7b1d7560a38d576bc38a28e10a240fdd2f
Nicole Theurer <nicole.theurer@outlook.com>
2018-03-15 16:18:06 -0600

    reset server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/items', (req, res) => {
 
 app.post('/api/items', (req, res) => {
  id = id + 1;
- let item = {id:id, text:req.body.text, completed: req.body.completed, rating: req.body.rating};
+ let item = {id:id, text:req.body.text, completed: req.body.completed, priority: req.body.priority};
  items.push(item);
  res.send(item);
 });
@@ -28,7 +28,7 @@ app.put('/api/items/:id', (req, res) => {
   let item = items[index];
   item.completed = req.body.completed;
   item.text = req.body.text;
-  item.rating = req.body.rating
+  item.priority = req.body.priority
   // handle drag and drop re-ordering
   if (req.body.orderChange) {
     let indexTarget = itemsMap.indexOf(req.body.orderTarget);
